d3fe3c009a630a70b09ecd3cf63269feb9bb7235
Leticia Sartori <leticia.m.sartori@usp.br>
2023-10-15 19:36:54 -0300

    add ex 2

---
+++ bash_script.sh
@@ -0,0 +1,12 @@
+#Exercício 2
+#- Um comando que mostre a pasta/diretório onde você está
+
+pwd
+
+#Um comando que gere uma lista com detalhes do conteúdo da sua pasta HOME e redirecione essa lista para o arquivo: lista_HOME.txt
+
+ls ~/ > lista_HOME.txt
+
+#- Um comando que imprima a data atual na tela.
+
+date
